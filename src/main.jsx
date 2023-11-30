3bd95a56ea5272ebe2d73ec3d381846d5bf4e233
waleed <waleed.sukhon77@gmail.com>
2023-11-30 18:45:40 +0200

    client page

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ColorModeScript } from "@chakra-ui/react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 import theme from "./theme";
 import "./index.css";
+import EditMap from "./pages/EditMap/EditMap";
+import ClientPage from "./pages/ClientPage/ClientPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "client",
+    element: <ClientPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
     <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <App />
+    <RouterProvider router={router} />
   </>
 );
---
+++ src/pages/ClientPage/ClientPage.jsx
@@ -0,0 +1,82 @@
+import { useState, useEffect } from "react";
+import { Spinner, Text } from "@chakra-ui/react";
+
+export default () => {
+  const [city, setCity] = useState();
+  const [data, setData] = useState();
+  const [info, setInfo] = useState({ city: "", cost: 0, available: true });
+
+  useEffect(() => {
+    // Check if the browser supports Geolocation
+    if ("geolocation" in navigator) {
+      // Get the user's location
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+          setInfo({ ...info, latitude, longitude });
+        },
+        (error) => {
+          console.error(`Error getting location: ${error.message}`);
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  }, []); // Empty dependency array ensures the effect runs once after component mount
+
+  useEffect(() => {
+    fetch("https://h-t-apps-backend-task.onrender.com/").then(
+      async (response) => {
+        const data = await response.json();
+        setData(data.data);
+      }
+    );
+  }, []);
+  function isPointInsideRegion(point, regionPoints) {
+    let inside = false;
+
+    for (
+      let i = 0, j = regionPoints.length - 1;
+      i < regionPoints.length;
+      j = i++
+    ) {
+      const xi = regionPoints[i].lat;
+      const yi = regionPoints[i].lng;
+      const xj = regionPoints[j].lat;
+      const yj = regionPoints[j].lng;
+
+      const intersect =
+        yi > point.lng !== yj > point.lng &&
+        point.lat < ((xj - xi) * (point.lng - yi)) / (yj - yi) + xi;
+
+      if (intersect) {
+        inside = !inside;
+      }
+    }
+
+    return inside;
+  }
+
+  useEffect(() => {
+    if (data)
+      data.forEach((city) => {
+        const isInside = isPointInsideRegion(
+          { lat: info.latitude, lng: info.longitude },
+          city.locations
+        );
+        if (isInside) {
+          setCity(city);
+        }
+      });
+  }, [data]);
+  if (!city) {
+    return <Spinner />;
+  }
+  return (
+    <div style={{ textAlign: "center", marginTop: "10px" }}>
+      <Text>you exist in {city.name}</Text>
+      <Text>delivery costs {city.cost}</Text>
+    </div>
+  );
+};
